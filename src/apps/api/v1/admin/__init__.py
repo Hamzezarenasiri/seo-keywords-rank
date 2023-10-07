83a0588e8f909c8dd97857b957951b8f761990f9
Hamze <hamzezn@gmail.com>
2023-10-08 00:10:19 +0330

    :art: disable endpoints

--- src/apps/api/v1/admin/__init__.py
+++ src/apps/api/v1/admin/__init__.py
@@ -10,13 +10,13 @@ from .users import user_router
 
 admin_router = APIRouter()
 
-admin_router.include_router(admin_auth_router, prefix="/auth")
+# admin_router.include_router(admin_auth_router, prefix="/auth")
 admin_router.include_router(keyword_router, prefix="/keywords")
 # admin_router.include_router(config_router, prefix="/configs")
 # admin_router.include_router(device_router, prefix="/devices")
 # admin_router.include_router(file_router, prefix="/files")
 # admin_router.include_router(group_router, prefix="/groups")
-admin_router.include_router(profile_router, prefix="/me")
+# admin_router.include_router(profile_router, prefix="/me")
 # admin_router.include_router(language_router, prefix="/languages")
 # admin_router.include_router(user_router, prefix="/users")
 # admin_router.include_router(state_router, prefix="/states")
